622df9d5faa017948d1f51ddc1d931b67f73e82a
hvitek@seznam.cz <hvitek@seznam.cz>
2021-03-29 21:05:45 +0200

    added Exception signature that method may fail if any input parameter is zero

--- src/main/java/app/Factorial.java
+++ src/main/java/app/Factorial.java
@@ -4,9 +4,13 @@ import java.math.BigInteger;
 import java.util.List;
 
 /**
- *  INPUT: List of integers
- *  OUTPUT: List of factorials in the same order
+ * INPUT: List of integers
+ * OUTPUT: List of factorials in the same order
  */
 public interface Factorial {
-    List<BigInteger> compute(List<Integer> inputNumbers);
+
+    /**
+     * @throws NumberIsLessThanZeroException If any of inputNumbers is a negative number...
+     */
+    List<BigInteger> compute(List<Integer> inputNumbers) throws NumberIsLessThanZeroException;
 }
