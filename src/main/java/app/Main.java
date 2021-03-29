aa24a37e0d6cf19e852149a384a86b6499057c69
hvitek@seznam.cz <hvitek@seznam.cz>
2021-03-29 22:01:17 +0200

    added USAGE printHelp()

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -9,7 +9,7 @@ import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.util.List;
 
-/**
+/**<pre>
  * This program computes factorial of all integers read from an input file.
  *
  * Each line in the input file contains just one input integer:
@@ -26,12 +26,14 @@ import java.util.List;
  * 7! = 5040
  *
  * - multithreading + no repeated computations
+ *
+
+ * </pre>
  */
 public class Main {
 
-    public static void main(String [] args) {
-        if (args.length != 1) {
-            System.err.println("This program requires one parameter. Path to an input file.");
+    public static void main(String[] args) {
+        if (!processCommandLine(args)) {
             return;
         }
 
@@ -42,16 +44,74 @@ public class Main {
 
         try {
             List<Integer> inputNumbers = loader.load(Path.of(pathToFileArg));
+
             List<BigInteger> output = factorial.compute(inputNumbers);
-            for(int i=0; i<inputNumbers.size(); i++) {
+            for (int i = 0; i < inputNumbers.size(); i++) {
                 System.out.println(inputNumbers.get(i) + "! = " + output.get(i));
             }
-        } catch (IOException | InvalidPathException  e) {
+        } catch (IOException | InvalidPathException e) {
             System.err.println("The parameter is not valid path. " + e.getMessage());
-        } catch (NumberFormatException e) {
-            System.err.println("The input file does not have expected format. Unrecognized value: " + e.getMessage());
-        } catch (ParallelException e) {
+        } catch (InvalidLineException e) {
+            ifInputNumberIsBiggerThanIntegerPrintRelevantMessageOtherwisePrintGeneralError(e);
+        } catch (ParallelException | NumberIsLessThanZeroException e) {
             System.err.println(e.getMessage());
+        }
+    }
+
+    /**
+     * https://picocli.info/ seems pretty good for java command line applications.
+     * However lets just print Help manually
+     *
+     * Returns false if any error has been encountered during command line parsing OR
+     *               the program should exist gracefully (-h argument).
+     * Returns true on success
+     */
+    private static boolean processCommandLine(String[] args) {
+        if (args.length != 1) {
+            System.err.println("This program requires just one cmd argument");
+            System.err.println(getHelpMessage());
+            return false;
+        } else if ("-h".equals(args[0]) || "--help".equals(args[0])) {
+            System.out.println(getHelpMessage());
+            return false;
+        } else if (args[0].startsWith("-")) {
+            System.err.println("Unrecognized cmd argument: " + args[0]);
+            System.err.println(getHelpMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    private static String getHelpMessage() {
+        StringBuilder helpBuilder = new StringBuilder();
+        helpBuilder.append("Usage: java -jar factorial.jar <file>\n\n");
+        helpBuilder.append("This program computes factorial of all integers read from an input file and prints results to STDOUT.\n");
+        helpBuilder.append("One line in the input file represents one number. The number must be in a range <0; 2,147,483,647>\n");
+        helpBuilder.append("  <file>        The input file containing one number on each line.\n");
+        helpBuilder.append("  -h, --help    Show this message and exit.\n");
+        return helpBuilder.toString();
+    }
+
+    /**
+     * FileNumberLoader parses input file line by line looking for Integers
+     * If it fails to parse current line it throws InvalidLineException
+     * It uses Integer.parseInt() method. Therefore it fails for numbers > Integer.MAX_VALUE or numbers < Integer.MIN_VALUE.
+     *
+     * This method ensures that in such a case it prints a unique message saying that an input number is too large.
+     * Any big negative number (>Integer.MIN_VALUE) falls also into this category...
+     *
+     * It uses BigInteger constructor to parse this potential line. Another solution may be in regular expressions (\d+)
+     */
+    private static void ifInputNumberIsBiggerThanIntegerPrintRelevantMessageOtherwisePrintGeneralError(InvalidLineException invalidLineException) {
+        try {
+            new BigInteger(invalidLineException.getInvalidLine());
+            System.err.println(
+                    "The input number: <" + invalidLineException.getInvalidLine() +
+                            "> is too large. Maximum supported number is: " + Integer.MAX_VALUE
+            );
+        } catch (NumberFormatException e) {
+            System.err.println(invalidLineException.getMessage());
         }
     }
 }
