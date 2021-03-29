64139c92b71f2175062db48da1fb1e2b16c05654
hvitek@seznam.cz <hvitek@seznam.cz>
2021-03-29 21:06:58 +0200

    added charset to FileReader + refactor to throw custom InvalidLineException

--- src/main/java/app/FileNumberLoader.java
+++ src/main/java/app/FileNumberLoader.java
@@ -3,27 +3,39 @@ package app;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileNumberLoader {
     /**
+     *  Parses an input file line by line looking for Integers
+     *  If it fails to parse the current line it throws InvalidLineException(lineNumber, line).
+     *  It internally uses Integer.parseInt() method
+     *
      * @throws IOException
-     * @throws NumberFormatException if cannot parse input file line as Integer...
+     * @throws InvalidLineException if cannot parse input file line as an Integer...
      */
-    public List<Integer> load(Path pathToFile) throws IOException, NumberFormatException {
-        try (BufferedReader br = new BufferedReader(new FileReader(pathToFile.toFile()))) {
+    public List<Integer> load(Path pathToFile) throws IOException, InvalidLineException {
+
+        int lineCount = 0;
+        String lineExpectedNumber = null;
+
+        try (BufferedReader br = new BufferedReader(
+                new FileReader(pathToFile.toFile(), StandardCharsets.UTF_8))
+        ) {
             List<Integer> output = new ArrayList<>();
 
-            String lineExpectedNumber;
             while ((lineExpectedNumber = br.readLine()) != null) {
-                    int number = Integer.parseInt(lineExpectedNumber);
-                    output.add(number);
-
+                lineCount++;
+                int number = Integer.parseInt(lineExpectedNumber);
+                output.add(number);
             }
 
             return output;
+        } catch (NumberFormatException e) {
+            throw new InvalidLineException(lineCount, lineExpectedNumber);
         }
     }
 }
